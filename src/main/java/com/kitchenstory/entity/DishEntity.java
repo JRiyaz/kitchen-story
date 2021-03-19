b09df01d3b38c747373e58dce5818b7288e9a67c
Riyaz J <j.riyazu@gmail.com>
2021-03-19 14:32:39 +0530

    FD: Add DishEntity, DishRepository, and DishService JPA entity classes.

---
+++ src/main/java/com/kitchenstory/entity/DishEntity.java
@@ -0,0 +1,75 @@
+package com.kitchenstory.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Data
+@DynamicUpdate
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "dish")
+public class DishEntity implements Serializable {
+
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "uuid2")
+    private String id;
+
+    @Column(length = 50, nullable = false)
+    @NotNull(message = "Dish name cannot be null")
+    private String name;
+
+    @Column(length = 50, nullable = false)
+    @NotNull(message = "Dish type cannot be null")
+    private String type;
+
+    @Column(length = 50, nullable = false)
+    @NotNull(message = "Spicy field cannot be null")
+    private String spicy;
+
+    @Column(length = 10, nullable = false)
+    @NotNull(message = "Price cannot be null")
+    @Min(value = 100, message = "Price must be minimum of 100")
+    private Double price;
+
+    @Column(length = 3, nullable = false)
+    @NotNull(message = "Rating cannot be null")
+    private Double rating;
+
+    @Column(length = 50, nullable = false)
+    @NotNull(message = "Normal field cannot be null")
+    private String normal;
+
+    @Transient
+    private String url;
+
+    @Lob
+    @Column(length = Integer.MAX_VALUE)
+    private byte[] image;
+
+    @Column(length = 1000)
+    private String description;
+
+    public DishEntity(String name, String type, String spicy, Double price, Double rating, String normal,
+                      byte[] image, String description) {
+        this.name = name;
+        this.type = type;
+        this.spicy = spicy;
+        this.price = price;
+        this.rating = rating;
+        this.normal = normal;
+        this.image = image;
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/kitchenstory/service/DishService.java
@@ -0,0 +1,42 @@
+package com.kitchenstory.service;
+
+import com.kitchenstory.entity.DishEntity;
+import com.kitchenstory.repository.DishRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class DishService {
+
+    private final DishRepository repository;
+
+    public List<DishEntity> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<DishEntity> findById(String id) {
+        return repository.findById(id);
+    }
+
+    public void deleteById(String id) {
+        repository.deleteById(id);
+    }
+
+    public DishEntity save(DishEntity dish) {
+        return repository.save(dish);
+    }
+
+    public List<DishEntity> saveAll(List dishes) {
+        return repository.saveAll(dishes);
+    }
+
+    public void deleteAll(List dishes) {
+        repository.deleteAll(dishes);
+    }
+}
