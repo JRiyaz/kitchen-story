2e847a35a2513087513adbc115e0a2acbf495d8a
Riyaz J <j.riyazu@gmail.com>
2021-03-21 09:23:04 +0530

    FO: Fix the null pointer exception in UniqueEmailIdValidator and UniqueDishNameValidator classes by adding BeanUtil class in config package, it supplies the beans required by both the classes.

--- src/main/java/com/kitchenstory/validator/UniqueDishNameValidator.java
+++ src/main/java/com/kitchenstory/validator/UniqueDishNameValidator.java
@@ -1,21 +1,38 @@
 package com.kitchenstory.validator;
 
-import com.kitchenstory.entity.DishEntity;
+import com.kitchenstory.config.BeanUtil;
 import com.kitchenstory.service.DishService;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.Optional;
 
+//https://stackoverflow.com/questions/37958145/autowired-gives-null-value-in-custom-constraint-validator
+//https://stackoverflow.com/questions/13350537/inject-service-in-constraintvalidator-bean-validator-jsr-303-spring
+//https://stackoverflow.com/questions/46594706/inject-repository-inside-constraintvalidator-with-spring-4-and-message-interpola
+//https://codingexplained.com/coding/java/hibernate/unique-field-validation-using-hibernate-spring
 public class UniqueDishNameValidator implements ConstraintValidator<UniqueDishName, String> {
 
-    @Autowired
     private DishService dishService;
+    private EntityManager entityManager;
+
+    @Override
+    public void initialize(UniqueDishName constraintAnnotation) {
+        dishService = BeanUtil.getBean(DishService.class);
+        entityManager = BeanUtil.getBean(EntityManager.class);
+    }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        final Optional<DishEntity> dish = dishService.findByName(value);
-        return dish.isPresent() ? false : true;
+        boolean isValid = false;
+        try {
+            entityManager.setFlushMode(FlushModeType.COMMIT);
+            //your code
+            isValid = dishService.findByName(value).isPresent() ? false : true;
+        } finally {
+            entityManager.setFlushMode(FlushModeType.AUTO);
+        }
+        return isValid;
     }
 }
--- src/main/java/com/kitchenstory/validator/UniqueEmailIdValidator.java
+++ src/main/java/com/kitchenstory/validator/UniqueEmailIdValidator.java
@@ -1,23 +1,38 @@
 package com.kitchenstory.validator;
 
-import com.kitchenstory.entity.UserEntity;
+import com.kitchenstory.config.BeanUtil;
 import com.kitchenstory.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.Optional;
 
+//https://stackoverflow.com/questions/37958145/autowired-gives-null-value-in-custom-constraint-validator
+//https://stackoverflow.com/questions/13350537/inject-service-in-constraintvalidator-bean-validator-jsr-303-spring
+//https://stackoverflow.com/questions/46594706/inject-repository-inside-constraintvalidator-with-spring-4-and-message-interpola
+//https://codingexplained.com/coding/java/hibernate/unique-field-validation-using-hibernate-spring
 public class UniqueEmailIdValidator implements ConstraintValidator<UniqueEmailId, String> {
 
-    @Autowired
     private UserService userService;
+    private EntityManager entityManager;
+
+    @Override
+    public void initialize(UniqueEmailId constraintAnnotation) {
+        userService = BeanUtil.getBean(UserService.class);
+        entityManager = BeanUtil.getBean(EntityManager.class);
+    }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-
-        final Optional<UserEntity> user = userService.findByEmail(value);
-
-        return user.isPresent() ? false : true;
+        boolean isValid = false;
+        try {
+            entityManager.setFlushMode(FlushModeType.COMMIT);
+            //your code
+            isValid = userService.findByEmail(value).isPresent() ? false : true;
+        } finally {
+            entityManager.setFlushMode(FlushModeType.AUTO);
+        }
+        return isValid;
     }
 }
